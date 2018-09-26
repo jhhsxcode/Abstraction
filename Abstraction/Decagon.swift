588fea4ed3e02c633575f943de9eeea1f598e97c
Paul.Jacks <pjacks9168@stu.d214.org>
2018-09-26 14:17:47 -0500

    added decagon class

---
+++ Abstraction/Decagon.swift
@@ -0,0 +1,13 @@
+//
+//  Decagon.swift
+//  Abstraction
+//
+//  Created by Paul Jacks on 9/26/18.
+//  Copyright © 2018 John Hersey High Schoo. All rights reserved.
+//
+
+import UIKit
+
+class Decagon{
+    
+}
