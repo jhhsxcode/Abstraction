9ba65a6df9f83ad1f7d1fdbb92cb56f636332619
Jansen.Rondael <prondael9481>
2018-09-26 14:00:38 -0500

    made Hexaigon class

---
+++ Hexaigon.swift
@@ -0,0 +1,13 @@
+//
+//  Hexaigon.swift
+//  Abstraction
+//
+//  Created by Jansen Rondael on 9/26/18.
+//  Copyright © 2018 John Hersey High Schoo. All rights reserved.
+//
+
+import UIKit
+
+class Hexaigon {
+    
+}
