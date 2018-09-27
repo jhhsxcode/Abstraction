3e254764ebb3f7ee03958334d93f2b6ec679e84e
Jhhs Xcode <jhhs.xcode@d214.org>
2018-09-27 07:44:46 -0500

    Made it so it won't crash if nothing entered

--- Abstraction/DrawView.swift
+++ Abstraction/DrawView.swift
@@ -10,13 +10,14 @@ import UIKit
 
 class DrawView: UIView {
     
-    public var vertices:[CGPoint] = []
+    public var vertices:[CGPoint] = [CGPoint()]
     public var lineColor: UIColor = UIColor.red
     
     public override func draw(_ rect: CGRect) {
         let context = UIGraphicsGetCurrentContext()!
         context.setLineWidth(2.0)
         context.setStrokeColor(lineColor.cgColor)
+
         for index in 0..<vertices.count-1 {
             context.move(to: vertices[index])
             context.addLine(to: vertices[index + 1])
