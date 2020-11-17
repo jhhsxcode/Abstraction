98cf44e4ba6bee559be20cc66b14069638df7939
Sarah Elenz <selenz3112@stu.d214.org>
2020-11-17 15:39:41 -0500

    Created vertices as CGPoints

--- Abstraction/Heptagon.swift
+++ Abstraction/Heptagon.swift
@@ -2,7 +2,18 @@
 //  Heptagon.swift
 //  Abstraction
 //
+import UIKit
 
 class Heptagon {
+    var vertice1: CGPoint
+    var vertice2: CGPoint
+    var vertice3: CGPoint
+    var vertice4: CGPoint
+    var vertice5: CGPoint
+    var vertice6: CGPoint
+    var vertice7: CGPoint
     
+    init() {
+        
+    }
 }
