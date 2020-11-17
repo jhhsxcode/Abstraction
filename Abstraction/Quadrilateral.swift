fb59f224eaf0f940c638d4d18e38499c47bf1ca5
jhhsxcode <32098478+jhhsxcode@users.noreply.github.com>
2020-11-17 15:24:57 -0600

    Update Quadrilateral.swift

--- Abstraction/Quadrilateral.swift
+++ Abstraction/Quadrilateral.swift
@@ -6,15 +6,6 @@ import UIKit
 
 class Quadrilateral{
     
-    var vertices: [CGPoint]
-    var lineColor: UIColor
-    var shapeName: String
     
-    init(vertices: [CGPoint]) {
-        self.vertices = vertices
-        
-        lineColor = .red
-        shapeName = "Quadrilateral"
-    }
     
 }
